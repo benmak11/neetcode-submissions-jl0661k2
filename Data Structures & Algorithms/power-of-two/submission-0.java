76055426345d54d193e8a19f9d4ca149ba0ffc5b
Ben Makusha <benmak11@users.noreply.github.com>
2026-08-03 22:56:13 -0400

    Add: power-of-two - submission-0

---
+++ Data Structures & Algorithms/power-of-two/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0)
+            return false;
+        
+        long x = 1;
+        while (x < n)
+            x *= 2;
+        
+        return x == n;
+    }
+}
